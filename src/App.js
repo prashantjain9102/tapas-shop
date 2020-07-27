7aa913b4ef84b80147a0e06f84ae3aa004035fb9
unknown <Admin@EMLP-MUM0016.aurum.com>
2020-07-27 14:45:51 +0545

    adding header component to the shop

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/hompage/homepage.component';
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header/>
+      <Switch>
     <Route exact path="/" component={HomePage}/>
     <Route path="/shop" component={Shop}/>
+      </Switch>
     </div>
   );
 }
